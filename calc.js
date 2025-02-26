538c788dd8bfb399ffb15cb55ec9fd6579daff6c
Amos Baya <amosbaya96@gmail.com>
2025-02-26 12:56:13 +0300

    calculator here

---
+++ calc.js
@@ -0,0 +1,15 @@
+// javascript engine for the calculator
+function ageCalculator() {
+    var userinput = document.getElementById("DOB").value;
+    var dob = new Date(userinput);
+    if (userinput == null || userinput == "") {
+        document.getElementById("message").innerHTML = "Choose a date";
+        return false;
+    } else {
+        var month_diff = Date.now() - dob.getTime();
+        var age_dt = new Date(month_diff);
+        var year = age_dt.getUTCFullYear();
+        var age = Math.abs(year - 1970);
+        document.getElementById("result").innerHTML = "Your age is: "+ age + " years";
+    }
+}
